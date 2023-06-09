5704cc0c4722e82fa865e786e0da6489777a3d2c
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-06-09 22:26:30 +0300

    хз че не так

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+// удалить после ревью
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
